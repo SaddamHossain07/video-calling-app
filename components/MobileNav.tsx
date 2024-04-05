091957207cfecd13066f7d46a75fac11fda32eab
SaddamHossain07 <contact.saddam3@gmail.com>
2024-04-06 02:58:38 +0600

    navbar correction

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -35,7 +35,9 @@ const MobileNav = () => {
               height={32}
               className="max-sm:size-10"
             />
-            <p className="text-[26px] font-extrabold text-white">Doom</p>
+            <p className="text-[26px] font-extrabold text-white">
+              Saddam | Room
+            </p>
           </Link>
 
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
